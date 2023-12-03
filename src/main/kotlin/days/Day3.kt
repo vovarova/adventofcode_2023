3d7dcb194dd33d37e988198b27d2188503891f25
Volodymyr Roman <volodymyr.roman@tui.be>
2023-12-03 14:58:20 +0200

    Day 3 Refactored

--- src/main/kotlin/days/Day3.kt
+++ src/main/kotlin/days/Day3.kt
@@ -5,89 +5,68 @@ import util.Matrix
 
 class Day3 : Day(3) {
 
-    override fun partOne(): Any {
-        val matrix = Matrix(inputListPart1.map {
+    fun gridNumbers(input: List<String>): MutableList<Pair<Int, List<GridCell<Char>>>> {
+        val matrix = Matrix(input.map {
             it.toCharArray().toTypedArray()
         }.toTypedArray())
-
-        val nonValidNumbers = mutableListOf<Int>()
-        val validNumbers = mutableListOf<Int>()
-
+        val gridNumbers = mutableListOf<Pair<Int, List<GridCell<Char>>>>()
         val numberParts = mutableListOf<GridCell<Char>>()
-
-        fun check() {
-            if (numberParts.isNotEmpty()) {
-
-                val checkNumber = numberParts.any {
-                    it.diagonalNeighbours().any {
-                        it.value != '.' && !it.value.isDigit()
-                    } || it.straightNeighbours().any {
-                        it.value != '.' && !it.value.isDigit()
-                    }
-                }
-                val number = numberParts.map { it.value }.joinToString("").toInt()
-                if (!checkNumber) {
-                    nonValidNumbers.add(number)
-                } else {
-                    validNumbers.add(number)
-                }
-                numberParts.clear()
-            }
-
-        }
-
         for (row in matrix.matrixGrid.indices) {
             for (column in matrix.matrixGrid[row].indices) {
                 if (matrix.cell(row, column).value.isDigit()) {
                     numberParts.add(matrix.cell(row, column))
                 } else {
-                    check()
+                    if (numberParts.isNotEmpty()) {
+                        gridNumbers.add(
+                            Pair(
+                                numberParts.map { it.value }.joinToString("").toInt(),
+                                listOf(*numberParts.toTypedArray())
+                            )
+                        )
+                        numberParts.clear()
+                    }
                 }
             }
-            check()
+            if (numberParts.isNotEmpty()) {
+                gridNumbers.add(
+                    Pair(
+                        numberParts.map { it.value }.joinToString("").toInt(),
+                        listOf(*numberParts.toTypedArray())
+                    )
+                )
+                numberParts.clear()
+            }
+        }
+        return gridNumbers;
+    }
+
+    override fun partOne(): Any {
+        val validNumbers = gridNumbers(inputListPart1).filter {
+            it.second.any {
+                it.diagonalNeighbours().any {
+                    it.value != '.' && !it.value.isDigit()
+                } || it.straightNeighbours().any {
+                    it.value != '.' && !it.value.isDigit()
+                }
+            }
         }
 
-        return validNumbers.sum()
+        return validNumbers.map { it.first }.sum()
     }
 
     override fun partTwo(): Any {
-        val matrix = Matrix(inputListPart2.map {
-            it.toCharArray().toTypedArray()
-        }.toTypedArray())
-
-        val starCells = mutableMapOf<GridCell<Char>, MutableList<Int>>()
-        val numberParts = mutableListOf<GridCell<Char>>()
-
-        fun check() {
-            if (numberParts.isNotEmpty()) {
-                val number = numberParts.map { it.value }.joinToString("").toInt()
-                val flatMap = numberParts.flatMap {
-                    mutableListOf<GridCell<Char>>().also { elements ->
-                        elements.addAll(it.straightNeighbours())
-                        elements.addAll(it.diagonalNeighbours())
-                    }
-                }.distinct().filter { it.value == '*' }.forEach {
-                    starCells.putIfAbsent(it, mutableListOf())
-                    starCells[it]?.add(number)
+        return gridNumbers(inputListPart2).flatMap {
+            it.second.flatMap {
+                mutableListOf<GridCell<Char>>().also { elements ->
+                    elements.addAll(it.straightNeighbours())
+                    elements.addAll(it.diagonalNeighbours())
                 }
-
-                numberParts.clear()
+            }.distinct().filter { it.value == '*' }.map { start ->
+                Pair(start, it.first)
             }
-
-        }
-
-        for (row in matrix.matrixGrid.indices) {
-            for (column in matrix.matrixGrid[row].indices) {
-                if (matrix.cell(row, column).value.isDigit()) {
-                    numberParts.add(matrix.cell(row, column))
-                } else {
-                    check()
-                }
-            }
-            check()
-        }
-
-        return starCells.filter { it.value.size>1 }.map { it.value.reduce { acc, i -> acc * i } }.sum()
+        }.groupBy { it.first }.filter { it.value.size > 1 }.map {
+            it.value.map { it.second }.reduce { acc, i -> acc * i }
+        }.sum()
     }
 }
 
