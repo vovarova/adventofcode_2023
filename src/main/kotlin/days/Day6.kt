12326f1e4496c682c0380e444f74b1182c7d73b2
Volodymyr Roman <volodymyr.roman@tui.be>
2023-12-06 22:49:57 +0200

    Day6 part 2 done

--- src/main/kotlin/days/Day6.kt
+++ src/main/kotlin/days/Day6.kt
@@ -9,7 +9,7 @@ class Day6 : Day("6") {
     Time:      7  15   30
 Distance:  9  40  200
      */
-    class RecordTime(val time: Int, val record: Int) {
+    class RecordTime(val time: Long, val record: Long) {
 
 
     }
@@ -17,9 +17,10 @@ Distance:  9  40  200
     override fun partOne(dayInput: DayInput): Any {
         val inputList = dayInput.inputList()
         val time =
-            inputList[0].replace("Time:", "").split(" ").map { it.trim() }.filter { it.isNotEmpty() }.map { it.toInt() }
+            inputList[0].replace("Time:", "").split(" ").map { it.trim() }.filter { it.isNotEmpty() }
+                .map { it.toLong() }
         val record = inputList[1].replace("Distance:", "").split(" ").map { it.trim() }.filter { it.isNotEmpty() }
-            .map { it.toInt() }
+            .map { it.toLong() }
 
         val recordTimeRecords = time.mapIndexed { index, timeItem ->
             RecordTime(timeItem, record[index])
@@ -39,8 +40,25 @@ Distance:  9  40  200
         return map.reduce { acc, i -> acc * i }
     }
 
+    fun raceResult(button: Long, time: Long): Long {
+        return button * (time - button)
+    }
+
     override fun partTwo(dayInput: DayInput): Any {
-        return dayInput.inputList()
+        val inputList = dayInput.inputList()
+        val time =
+            inputList[0].replace("Time:", "").split(" ").map { it.trim() }.filter { it.isNotEmpty() }.joinToString("")
+                .toLong()
+        val record = inputList[1].replace("Distance:", "").split(" ").map { it.trim() }.filter { it.isNotEmpty() }
+            .joinToString("").toLong()
+
+        val disriminant = time * time - 4 * record
+        val x1 = Math.ceil((time.toDouble() - Math.sqrt(disriminant.toDouble())) / 2.toDouble()).toLong()
+        val x2 = Math.floor((time.toDouble() + Math.sqrt(disriminant.toDouble())) / 2.toDouble()).toLong()
+        var result = x2 - x1 - 1
+        if (raceResult(x1, time) > record) result++
+        if (raceResult(x2, time) > record) result++
+        return result
     }
 }
 
